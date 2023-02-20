d18203d52f22b4a65174356b895f3e4523241673
Reid Mason <maddogshain132@gmail.com>
2023-02-20 10:23:33 +0000

    Added whichkey

--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -1,14 +1,15 @@
 return {
+	--vim.lsp.but.format()
 	"folke/which-key.nvim",
 	config = function()
 		local keybinds = {
-			["w"] = { "<cmd>w!<CR>", "Save"},
+			["w"] = { "<cmd>w!<CR>", "Save" },
 			["q"] = { "<cmd>confirm q<CR>", "Quit" },
 			["h"] = { "<cmd>nohlsearch<CR>", "No Highlight" },
-		} 
-		local opts = {mode = "n", prefix = "<leader>"}
+			["f"] = { "<cmd>Telescope find_files<CR>", "Find files" },
+			["b"] = { "<cmd>Telescope buffers<CR>", "Open buffers" }
+		}
+		local opts = { mode = "n", prefix = "<leader>" }
 		require("which-key").register(keybinds, opts)
 	end
 }
-
-
