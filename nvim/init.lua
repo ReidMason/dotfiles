be76eace26be0e166314ec7d297d485eb7d5b3ba
Reid Mason <maddogshain132@gmail.com>
2023-02-20 10:22:58 +0000

    Added format on save and highlight on yank

--- nvim/init.lua
+++ nvim/init.lua
@@ -14,7 +14,18 @@ vim.opt.rtp:prepend(lazypath)
 
 -- Set leader key to space
 vim.keymap.set("n", "<Space>", "<Nop>", { silent = true, remap = false })
-vim.g.mapleader = " " 
+vim.g.mapleader = " "
+
+-- Auto format on save
+vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()]]
+
+-- Hilight on yank
+vim.cmd[[
+augroup highlight_yank
+autocmd!
+au TextYankPost * silent! lua vim.highlight.on_yank({higroup="Search", timeout=100})
+augroup END
+]]
 
 -- Make vim use the native clipboard so vim and the machine share the same clipboard
 vim.opt.clipboard = "unnamedplus"
@@ -22,6 +33,8 @@ vim.opt.clipboard = "unnamedplus"
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
 vim.opt.relativenumber = true
+vim.opt.number = true
+
 
 -- Setup lazy.nvim
 -- Here we tell lazy.nvim to load the contents of the lua/plugins/init.lua file
