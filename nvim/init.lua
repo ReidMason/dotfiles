e34e5418520207d514714b90cde30a8fae329f76
Reid Mason <maddogshain132@gmail.com>
2023-03-02 03:49:39 +0000

    Fixed autoformat and other features

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,31 +1,58 @@
 -- Install lazy.nvim our packagemanager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- Set leader key to space
 vim.keymap.set("n", "<Space>", "<Nop>", { silent = true, remap = false })
 vim.g.mapleader = " "
+vim.g.maplocalleader = ' '
 
 -- Auto format on save
-vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()]]
+-- vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.format()]]
+vim.api.nvim_create_autocmd("BufWritePre", {
+	-- pattern = opts.pattern,
+	callback = function()
+		vim.lsp.buf.format()
+	end,
+})
+
+-- Save undo history
+vim.o.undofile = true
+
+-- Case insensitive searching UNLESS /C or capital in search
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Keep signcolumn on by default (Stops the whole window shifting every time signs are added to the gutter)
+vim.wo.signcolumn = 'yes'
+
+-- NOTE: You should make sure your terminal supports this
+vim.o.termguicolors = true
+
+-- Decrease update time
+vim.o.updatetime = 250
+vim.o.timeout = true
+vim.o.timeoutlen = 300
 
 -- Hilight on yank
-vim.cmd[[
-augroup highlight_yank
-autocmd!
-au TextYankPost * silent! lua vim.highlight.on_yank({higroup="Search", timeout=100})
-augroup END
-]]
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+	callback = function()
+		vim.highlight.on_yank({ timeout = 100 })
+	end,
+	group = highlight_group,
+	pattern = '*',
+})
 
 -- Make vim use the native clipboard so vim and the machine share the same clipboard
 vim.opt.clipboard = "unnamedplus"
@@ -38,4 +65,6 @@ vim.opt.number = true
 
 -- Setup lazy.nvim
 -- Here we tell lazy.nvim to load the contents of the lua/plugins/init.lua file
-require("lazy").setup("plugins")
+require("lazy").setup("plugins", {
+	change_detection = { enabled = false }
+})
