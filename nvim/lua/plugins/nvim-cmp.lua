1402f18ef7224802bdd1cc6a0b8be6823a542717
Reid Mason <maddogshain132@gmail.com>
2023-03-03 00:46:21 +0000

    Started fixing cmp

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -1,26 +1,81 @@
 return {
 	"hrsh7th/nvim-cmp",
 	dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip',
-	"hrsh7th/cmp-buffer",
-	"hrsh7th/cmp-path",
-	"hrsh7th/cmp-cmdline",
-	"rafamadriz/friendly-snippets",
-},
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-cmdline",
+		"rafamadriz/friendly-snippets",
+	},
 	config = function()
-			local cmp = require("cmp")
-			local luasnip = require("luasnip")
-			luasnip.config.setup({})
+		local cmp = require("cmp")
+		local cmp_window = require "cmp.config.window"
+		local luasnip = require("luasnip")
 
-			cmp.setup({
-				snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end,
-  },
-	sources = {
-		{name = "nvim_lsp"},
-		{name = "luasnip"}
-	}
-			})
+		-- local cmp_window = require "cmp.config.window"
+		local cmp_mapping = require "cmp.config.mapping"
+		luasnip.config.setup({})
+
+		cmp.setup({
+			snippet = {
+				expand = function(args)
+					luasnip.lsp_expand(args.body)
+				end,
+			},
+			window = {
+				completion = cmp_window.bordered(),
+				documentation = cmp_window.bordered(),
+			},
+			mapping = cmp_mapping.preset.insert {
+				['<C-d>'] = cmp.mapping.scroll_docs( -4),
+				['<C-f>'] = cmp.mapping.scroll_docs(4),
+				['<C-Space>'] = cmp.mapping.complete {},
+				['<CR>'] = cmp.mapping.confirm {
+					behavior = cmp.ConfirmBehavior.Replace,
+					select = true,
+				},
+				['<Tab>'] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_next_item()
+					elseif luasnip.expand_or_jumpable() then
+						luasnip.expand_or_jump()
+					else
+						fallback()
+					end
+				end, { 'i', 's' }),
+				['<S-Tab>'] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_prev_item()
+					elseif luasnip.jumpable( -1) then
+						luasnip.jump( -1)
+					else
+						fallback()
+					end
+				end, { 'i', 's' }),
+			},
+			sources = {
+				{
+					name = "nvim_lsp",
+					entry_filter = function(entry, ctx)
+						local kind = require("cmp.types.lsp").CompletionItemKind[entry:get_kind()]
+						if kind == "Snippet" and ctx.prev_context.filetype == "java" then
+							return false
+						end
+						if kind == "Text" then
+							return false
+						end
+						return true
+					end,
+				},
+				{ name = "path" },
+				{ name = "luasnip" },
+				{ name = "nvim_lua" },
+				{ name = "buffer" },
+				{ name = "calc" },
+				{ name = "emoji" },
+				{ name = "treesitter" },
+				{ name = "crates" },
+				{ name = "tmux" },
+			}
+		})
 	end
 }
