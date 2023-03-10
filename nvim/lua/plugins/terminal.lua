57d134c75c3d24378d77b12a404f6afaf8819ef8
Reid Mason <maddogshain132@gmail.com>
2023-03-10 03:11:46 +0000

    Changed terminal mapping to use Alt and added lazy loading

--- nvim/lua/plugins/terminal.lua
+++ nvim/lua/plugins/terminal.lua
@@ -1,16 +1,5 @@
 return {
 	"akinsho/toggleterm.nvim",
-	branch = "main",
-	config = function()
-		require("toggleterm").setup({
-			open_mapping = [[<c-\>]],
-			direction = "float",
-			float_opts = {
-				border = "curved",
-				winblend = 0,
-			},
-		})
-	end,
 	cmd = {
 		"ToggleTerm",
 		"TermExec",
@@ -19,5 +8,16 @@ return {
 		"ToggleTermSendVisualLines",
 		"ToggleTermSendVisualSelection",
 	},
-	keys = [[<c-\>]],
+	branch = "main",
+	config = function()
+		require("toggleterm").setup({
+			open_mapping = [[<M-\>]],
+			direction = "float",
+			float_opts = {
+				border = "curved",
+				winblend = 0,
+			},
+		})
+	end,
+	keys = [[<M-\>]],
 }
