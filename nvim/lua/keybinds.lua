505a1088374a3ac483f6f518a48590ea2b0f9339
Reid Mason <maddogshain132@gmail.com>
2023-06-16 01:25:52 +0100

    Removed all telescope requires for lazy loading

--- nvim/lua/keybinds.lua
+++ nvim/lua/keybinds.lua
@@ -11,7 +11,6 @@ vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = tr
 -- [[ Leader mapping ]]
 vim.keymap.set('n', '<leader>w', "<cmd>w!<CR>", { desc = 'Save' })
 vim.keymap.set('n', '<leader>c', '<cmd>:confirm bd<CR>', { desc = 'Close buffer' })
-vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
 
 -- Diagnostic navigation keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
@@ -20,13 +19,7 @@ vim.keymap.set('n', '<C-k>', vim.diagnostic.open_float, { desc = 'Open floating 
 
 vim.keymap.set('n', '<leader>qq', "<cmd>confirm q<CR>", { desc = 'Quit' })
 
-vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
-vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
-vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
-vim.keymap.set('n', '<leader>st', require('telescope.builtin').live_grep, { desc = '[S]earch [T]ext' })
-
-vim.keymap.set('n', 'gd', require('telescope.builtin').lsp_definitions)
-vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references)
+-- here
 
 vim.keymap.set('n', '<leader>la', vim.lsp.buf.code_action, { desc = "Code action" })
 vim.keymap.set('n', '<leader>lr', vim.lsp.buf.rename, { desc = "Rename" })
@@ -38,28 +31,3 @@ end
 
 vim.keymap.set('n', '<leader>/', "<Plug>(comment_toggle_linewise_current)", { desc = "Comment line" })
 vim.keymap.set('v', '<leader>/', "<Plug>(comment_toggle_linewise_visual)", { desc = "Comment line" })
-
---[[
-local keybinds = {
-	-- ["/"] = { "<Plug>(comment_toggle_linewise_current)", "Comment toggle current line" },
-	-- ["e"] = { "<cmd>NvimTreeToggle<CR>", "Open file tree" },
-	--["s"] = {
-		name = "Search",
-		t = { telescope.live_grep, "Text" },
-		f = { telescope.current_buffer_fuzzy_find, "Current buffer" }
-	},
---[[	
-	["l"] = {
-		name = "Lsp",
-		a = { "<cmd>lua vim.lsp.buf.code_action()<cr>", "Code Action" },
-		d = { telescope.diagnostics, "Buffer Diagnostics" },
-		r = { "<cmd>lua vim.lsp.buf.rename()<cr>", "Rename" },
-	},]]
--- }]]--
---local vkeybinds = {
--- ["/"] = { "<Plug>(comment_toggle_linewise_visual)", "Comment toggle linewise (visual)" },
---}
-
--- [[ Configure Telescope ]]
--- See `:help telescope` and `:help telescope.setup()`
--- require('telescope').setup
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,8 +1,19 @@
 return {
 	'nvim-telescope/telescope.nvim',
-	branch = '0.1.x',
-	dependencies = { 'nvim-lua/plenary.nvim', 		'nvim-telescope/telescope-fzf-native.nvim' },
-	event = "VeryLazy",
+	dependencies = { 'nvim-lua/plenary.nvim' },
+	cmd = "Telescope",
+	init = function()
+		vim.keymap.set('n', '<leader>sf', "<cmd> Telescope find_files <CR>", { desc = '[S]earch [F]iles' })
+		vim.keymap.set('n', '<leader>st', "<cmd> Telescope live_grep <CR>", { desc = '[S]earch [T]ext' })
+
+		vim.keymap.set('n', '<leader>?', "<cmd> Telescope oldfiles <CR>", { desc = '[?] Find recently opened files' })
+
+		vim.keymap.set('n', '<leader>sh', "<cmd> Telescope help_tags <CR>", { desc = '[S]earch [H]elp' })
+		vim.keymap.set('n', '<leader>sd', "<cmd> Telescope diagnostics <CR>", { desc = '[S]earch [D]iagnostics' })
+
+		vim.keymap.set('n', 'gd', "<cmd> Telescope lsp_definitions <CR>")
+		vim.keymap.set('n', 'gr', "<cmd> Telescope lsp_references <CR>")
+	end,
 	opts = {
 		defaults = {
 			mappings = {
