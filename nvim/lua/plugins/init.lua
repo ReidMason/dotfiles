01e82efc685d7efdb5a1573d41daef194aad2f21
Reid Mason <maddogshain132@gmail.com>
2023-06-16 01:26:53 +0100

    Cleaned up plugins

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,19 +1,27 @@
 local colourscheme = "tokyonight"
 return {
-	-- 'tpope/vim-fugitive',
+	{ 'nvim-lua/plenary.nvim' },
+	{
+		'tpope/vim-fugitive',
+		enabled = false
+	},
 	-- 'tpope/vim-rhubarb',
+	{
+		"sindrets/diffview.nvim",
+		enabled = false
+	},
 	{
 		-- Theme inspired by Atom
 		--'navarasu/onedark.nvim',
 		'folke/tokyonight.nvim',
 		priority = 1000,
-		config = function()
+		init = function()
 			vim.cmd.colorscheme(colourscheme)
 		end,
 	},
 	{
 		'akinsho/bufferline.nvim',
-		event = "VeryLazy",
+		event = "BufReadPost",
 		opts = {}
 	},
 	{
@@ -53,7 +61,6 @@ return {
 	{
 		-- Useful plugin to show you pending keybinds.
 		'folke/which-key.nvim',
-		event = "VeryLazy",
 		opts = {},
 	},
 	{
@@ -73,32 +80,26 @@ return {
 		-- Add indentation guides even on blank lines
 		'lukas-reineke/indent-blankline.nvim',
 		event = "VeryLazy",
+		enabled = false,
 		opts = {
 			char = '┊',
 			show_trailing_blankline_indent = false,
 		}
 	},
-	-- {
-	-- 	-- Adds git releated signs to the gutter, as well as utilities for managing changes
-	-- 	'lewis6991/gitsigns.nvim',
-	-- 	event = "VeryLazy",
-	-- 	opts = {
-	-- 		-- See `:help gitsigns.txt`
-	-- 		signs = {
-	-- 			add = { text = '+' },
-	-- 			change = { text = '~' },
-	-- 			delete = { text = '_' },
-	-- 			topdelete = { text = '‾' },
-	-- 			changedelete = { text = '~' },
-	-- 		},
-	-- 		on_attach = function(bufnr)
-	-- 			vim.keymap.set('n', '<leader>gp', require('gitsigns').prev_hunk,
-	-- 				{ buffer = bufnr, desc = '[G]o to [P]revious Hunk' })
-	-- 			vim.keymap.set('n', '<leader>gn', require('gitsigns').next_hunk,
-	-- 				{ buffer = bufnr, desc = '[G]o to [N]ext Hunk' })
-	-- 			vim.keymap.set('n', '<leader>ph', require('gitsigns').preview_hunk,
-	-- 				{ buffer = bufnr, desc = '[P]review [H]unk' })
-	-- 		end,
-	-- 	},
-	-- },
+	{
+		-- Adds git releated signs to the gutter, as well as utilities for managing changes
+		-- Gitsigns diffthis
+		'lewis6991/gitsigns.nvim',
+		enabled = false,
+		event = "VeryLazy",
+		opts = {
+			signs = {
+				add = { text = '+' },
+				change = { text = '~' },
+				delete = { text = '_' },
+				topdelete = { text = '‾' },
+				changedelete = { text = '~' },
+			},
+		},
+	},
 }
