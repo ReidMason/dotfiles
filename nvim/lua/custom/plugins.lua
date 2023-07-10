f5a2bfe66b7c8854cb15b0b4a88d69b8d50a1ceb
Reid Mason <maddogshain132@gmail.com>
2023-07-10 22:59:54 +0100

    Added QMK plugin for nvim

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -1,5 +1,21 @@
 local plugins = {
   {
+    "codethread/qmk.nvim",
+    event = "VeryLazy",
+    config = function()
+      local qmk = require "qmk"
+      qmk.setup {
+        name = "LAYOUT",
+        layout = { -- create a visual representation the final layout
+          "x x x x x x _ _ _ _ _ _ x x x x x x",
+          "x x x x x x _ _ _ _ _ _ x x x x x x",
+          "x x x x x x x x _ _ x x x x x x x x",
+          "_ _ _ x x x x x _ _ x x x x x _ _ _",
+        },
+      }
+    end,
+  },
+  {
     -- Can't get this to stage files though the diff view is good
     "sindrets/diffview.nvim",
     dependencies = {
