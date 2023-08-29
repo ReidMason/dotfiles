8a220c718f6035d149290bf75c0cede78403b8de
Reid Mason <maddogshain132@gmail.com>
2023-08-29 21:55:15 +0100

    Cleaned up nvim config

--- nvim/lua/custom/chadrc.lua
+++ nvim/lua/custom/chadrc.lua
@@ -10,15 +10,9 @@ M.ui = {
   tabufline = {
     show_numbers = true,
   },
-  cmp = {
-    -- style = "default", -- default/flat_light/flat_dark/atom/atom_colored
-    -- border_color = "folder_bg", -- only applicable for "default" style, use color names from base30 variables
-    -- selected_item_bg = "simple", -- colored / simple
-  },
   transparency = true,
   nvdash = {
     load_on_startup = true,
-
     header = {
       "           ▄ ▄                   ",
       "       ▄   ▄▄▄     ▄ ▄▄▄ ▄ ▄     ",
