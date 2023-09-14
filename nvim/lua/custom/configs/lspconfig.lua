f6666eb3b9fc0848fabd8ff1133793d219b68b69
Reid Mason <maddogshain132@gmail.com>
2023-09-14 20:40:28 +0100

    Added astro language server to configured LSPs

--- nvim/lua/custom/configs/lspconfig.lua
+++ nvim/lua/custom/configs/lspconfig.lua
@@ -2,7 +2,7 @@ local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "tsserver" }
+local servers = { "tsserver", "astro" }
 local util = require "lspconfig/util"
 
 for _, lsp in ipairs(servers) do
