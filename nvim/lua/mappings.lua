d946fb757c8489cdc9bb38484ba08bfd508c6b13
Reid Mason <maddogshain132@gmail.com>
2023-11-08 23:14:32 +0000

    Added command for selecting all lines

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -1,31 +1,31 @@
-
 local mappings = { n = {
-    ["<Esc>"] = { "<cmd> noh <CR>", "Clear highlights" },
+  ["<Esc>"] = { "<cmd> noh <CR>", "Clear highlights" },
 
-    ["<leader>w"] = { "<cmd> w! <CR>", "Save" },
-    ["<leader>qq"] = {"<cmd> confirm qa <CR>", "Quit" },
+  ["<leader>w"] = { "<cmd> w! <CR>", "Save" },
+  ["<leader>qq"] = {"<cmd> confirm qa <CR>", "Quit" },
+  ["<C-a>"] = { "ggVG", "Select all" },
 
-    -- Harpoon
-["<leader>a"] = { "<cmd> lua require('harpoon.mark').add_file() <CR>", "Harpoon file" },
-    ["<C-e>"] = { "<cmd> lua require('harpoon.ui').toggle_quick_menu() <CR>", "Toggle Harpon quick menu" },
+  -- Harpoon
+  ["<leader>a"] = { "<cmd> lua require('harpoon.mark').add_file() <CR>", "Harpoon file" },
+  ["<C-e>"] = { "<cmd> lua require('harpoon.ui').toggle_quick_menu() <CR>", "Toggle Harpon quick menu" },
 
--- Telescope
-    ["<leader>sf"] = { "<cmd> Telescope find_files <CR>", "Find files" },
-    ["<leader>st"] = { "<cmd> Telescope live_grep <CR>", "Search text" },
-    ["<leader>sd"] = { "<cmd> Telescope diagnostics <CR>", "Open diagnostics" },
-    ["gd"] = { "<cmd> Telescope lsp_definitions <CR>", "Find definition" },
-    ["gr"] = { "<cmd> Telescope lsp_references <CR>", "Find references" },
+  -- Telescope
+  ["<leader>sf"] = { "<cmd> Telescope find_files <CR>", "Find files" },
+  ["<leader>st"] = { "<cmd> Telescope live_grep <CR>", "Search text" },
+  ["<leader>sd"] = { "<cmd> Telescope diagnostics <CR>", "Open diagnostics" },
+  ["gd"] = { "<cmd> Telescope lsp_definitions <CR>", "Find definition" },
+  ["gr"] = { "<cmd> Telescope lsp_references <CR>", "Find references" },
 
-        -- Tmux navigation
-    ["<C-h>"] = { "<cmd> TmuxNavigateLeft <CR>", "Window left" },
-    ["<C-j>"] = { "<cmd> TmuxNavigateDown <CR>", "Window down" },
-    ["<C-k>"] = { "<cmd> TmuxNavigateUp <CR>", "Window up" },
-    ["<C-l>"] = { "<cmd> TmuxNavigateRight <CR>", "Window right" },
+  -- Tmux navigation
+  ["<C-h>"] = { "<cmd> TmuxNavigateLeft <CR>", "Window left" },
+  ["<C-j>"] = { "<cmd> TmuxNavigateDown <CR>", "Window down" },
+  ["<C-k>"] = { "<cmd> TmuxNavigateUp <CR>", "Window up" },
+  ["<C-l>"] = { "<cmd> TmuxNavigateRight <CR>", "Window right" },
 },
 x = {
-	-- Don't copy the replaced text after pasting in visual mode
-    -- https://vim.fandom.com/wiki/Replace_a_word_with_yanked_text#Alternative_mapping_for_paste
- ["p"] = { 'p:let @+=@0<CR>:let @"=@0<CR>', "Dont copy replaced text", opts = { silent = true } },
+  -- Don't copy the replaced text after pasting in visual mode
+  -- https://vim.fandom.com/wiki/Replace_a_word_with_yanked_text#Alternative_mapping_for_paste
+  ["p"] = { 'p:let @+=@0<CR>:let @"=@0<CR>', "Dont copy replaced text", opts = { silent = true } },
 }
 }
 
@@ -33,10 +33,10 @@ x = {
 -- vim.keymap.set("n", "<leader>-", vim.cmd.Ex)  
 
 for mode, values in pairs(mappings) do
-	for keybind, mapping_info in pairs(values) do
-		local opts = { desc = mapping_info[2]         }
-			vim.keymap.set(mode, keybind, mapping_info[1], opts)     
-	end 
+  for keybind, mapping_info in pairs(values) do
+    local opts = { desc = mapping_info[2]         }
+    vim.keymap.set(mode, keybind, mapping_info[1], opts)     
+  end 
 end
 
 -- Switch between buffers
