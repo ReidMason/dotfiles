58ed42f3d6afeaa1b8763a0eb133adbd9149d394
Reid Mason <maddogshain132@gmail.com>
2023-11-09 19:01:31 +0000

    Added more config values

--- nvim/lua/config.lua
+++ nvim/lua/config.lua
@@ -4,6 +4,7 @@ vim.cmd.colorscheme("catppuccin-mocha")
 vim.opt.clipboard = "unnamedplus"
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
+vim.o.pumheight = 10 -- Set popup menu height this works for autocompletion
 
 vim.opt.undofile = true
 vim.wo.relativenumber = true
@@ -15,3 +16,17 @@ vim.opt.shiftwidth = 2
 vim.opt.smartindent = true
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
+
+vim.opt.fillchars = { eob = " " }
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.mouse = "a"
+
+-- disable nvim intro
+vim.opt.shortmess:append("sI")
+
+vim.opt.signcolumn = "yes"
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.timeoutlen = 400
+vim.opt.undofile = true
--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -1,48 +1,60 @@
-local mappings = { n = {
-  ["<Esc>"] = { "<cmd> noh <CR>", "Clear highlights" },
+local mappings = {
+	n = {
+		["<Esc>"] = { "<cmd> noh <CR>", "Clear highlights" },
 
-  ["<leader>w"] = { "<cmd> w! <CR>", "Save" },
-  ["<leader>qq"] = {"<cmd> confirm qa <CR>", "Quit" },
-  ["<C-a>"] = { "ggVG", "Select all" },
+		["<leader>w"] = { "<cmd> w! <CR>", "Save" },
+		["<leader>qq"] = { "<cmd> confirm qa <CR>", "Quit" },
+		["<C-a>"] = { "ggVG", "Select all" },
+		["<leader>e"] = { "<cmd> NvimTreeToggle <CR>", "Toggle nvimtree" },
 
-  -- Harpoon
-  ["<leader>a"] = { "<cmd> lua require('harpoon.mark').add_file() <CR>", "Harpoon file" },
-  ["<C-e>"] = { "<cmd> lua require('harpoon.ui').toggle_quick_menu() <CR>", "Toggle Harpon quick menu" },
+		-- Harpoon
+		["<leader>a"] = { "<cmd> lua require('harpoon.mark').add_file() <CR>", "Harpoon file" },
+		["<C-e>"] = { "<cmd> lua require('harpoon.ui').toggle_quick_menu() <CR>", "Toggle Harpon quick menu" },
 
-  -- Telescope
-  ["<leader>sf"] = { "<cmd> Telescope find_files <CR>", "Find files" },
-  ["<leader>st"] = { "<cmd> Telescope live_grep <CR>", "Search text" },
-  ["<leader>sd"] = { "<cmd> Telescope diagnostics <CR>", "Open diagnostics" },
-  ["gd"] = { "<cmd> Telescope lsp_definitions <CR>", "Find definition" },
-  ["gr"] = { "<cmd> Telescope lsp_references <CR>", "Find references" },
+		-- Telescope
+		["<leader>sf"] = { "<cmd> Telescope find_files <CR>", "Find files" },
+		["<leader>st"] = { "<cmd> Telescope live_grep <CR>", "Search text" },
+		["<leader>sd"] = { "<cmd> Telescope diagnostics <CR>", "Open diagnostics" },
+		["gd"] = { "<cmd> Telescope lsp_definitions <CR>", "Find definition" },
+		["gr"] = { "<cmd> Telescope lsp_references <CR>", "Find references" },
 
-  -- Tmux navigation
-  ["<C-h>"] = { "<cmd> TmuxNavigateLeft <CR>", "Window left" },
-  ["<C-j>"] = { "<cmd> TmuxNavigateDown <CR>", "Window down" },
-  ["<C-k>"] = { "<cmd> TmuxNavigateUp <CR>", "Window up" },
-  ["<C-l>"] = { "<cmd> TmuxNavigateRight <CR>", "Window right" },
-},
-x = {
-  -- Don't copy the replaced text after pasting in visual mode
-  -- https://vim.fandom.com/wiki/Replace_a_word_with_yanked_text#Alternative_mapping_for_paste
-  ["p"] = { 'p:let @+=@0<CR>:let @"=@0<CR>', "Dont copy replaced text", opts = { silent = true } },
+		-- Tmux navigation
+		["<C-h>"] = { "<cmd> TmuxNavigateLeft <CR>", "Window left" },
+		["<C-j>"] = { "<cmd> TmuxNavigateDown <CR>", "Window down" },
+		["<C-k>"] = { "<cmd> TmuxNavigateUp <CR>", "Window up" },
+		["<C-l>"] = { "<cmd> TmuxNavigateRight <CR>", "Window right" },
+	},
+	x = {
+		-- Don't copy the replaced text after pasting in visual mode
+		-- https://vim.fandom.com/wiki/Replace_a_word_with_yanked_text#Alternative_mapping_for_paste
+		["p"] = { 'p:let @+=@0<CR>:let @"=@0<CR>', "Dont copy replaced text", opts = { silent = true } },
+	},
 }
-}
 
--- vim.g.mapleader = " " -- Set <space> as leader key 
--- vim.keymap.set("n", "<leader>-", vim.cmd.Ex)  
+-- vim.g.mapleader = " " -- Set <space> as leader key
+-- vim.keymap.set("n", "<leader>-", vim.cmd.Ex)
 
 for mode, values in pairs(mappings) do
-  for keybind, mapping_info in pairs(values) do
-    local opts = { desc = mapping_info[2]         }
-    vim.keymap.set(mode, keybind, mapping_info[1], opts)     
-  end 
+	for keybind, mapping_info in pairs(values) do
+		local opts = { desc = mapping_info[2] }
+		vim.keymap.set(mode, keybind, mapping_info[1], opts)
+	end
 end
 
 -- Switch between buffers
 for i = 1, 9, 1 do
-  vim.keymap.set("n", string.format("<leader>%s", i), function()
-    -- vim.api.nvim_set_current_buf(vim.t.bufs[i])
-    require("harpoon.ui").nav_file(i)
-  end)
+	vim.keymap.set("n", string.format("<leader>%s", i), function()
+		-- vim.api.nvim_set_current_buf(vim.t.bufs[i])
+		require("harpoon.ui").nav_file(i)
+	end)
 end
+
+-- Highlight on yank
+local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+vim.api.nvim_create_autocmd("TextYankPost", {
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+	group = highlight_group,
+	pattern = "*",
+})
