8b14a13c1aba5665c035128bc38b690c4616e63a
Reid Mason <maddogshain132@gmail.com>
2023-11-09 01:38:14 +0000

    Cleaned up plugins

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -1,56 +1,103 @@
 return {
-  {
-    "ThePrimeagen/harpoon",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-  },
+	{
+		"ThePrimeagen/harpoon",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+	},
 
-  {"NvChad/nvim-colorizer.lua",
-  opts = {
-    filetypes = {"*"},
-    user_default_options = {
-      names = false,
-      tailwind = true,
-      css = true,
-      css_fn = true,
-    },
-  }
-},
-{ "catppuccin/nvim",
-name = "catppuccin",
-priority = 1000,
-opts = {
-  transparent_background = true,
-  integrations = {
-    notify = true,
-    harpoon = true,
-  }
-}
-  },
+	{
+		"NvChad/nvim-colorizer.lua",
+		opts = {
+			filetypes = { "*" },
+			user_default_options = {
+				names = false,
+				tailwind = true,
+				css = true,
+				css_fn = true,
+			},
+		},
+	},
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		opts = {
+			transparent_background = true,
+			integrations = {
+				notify = true,
+				harpoon = true,
+			},
+		},
+	},
 
-  {
-    "nvim-treesitter/nvim-treesitter",
-    event = { "BufReadPost", "BufNewFile" },
-    cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
-    build = ":TSUpdate",
-    config = function()
-      require'nvim-treesitter.configs'.setup{
-        auto_install=true,
-        highlight = {
-          enable = true
-        }
-      }
-    end
-  },
-  {
-    'nvim-telescope/telescope.nvim',
-    cmd = {"Telescope"},
-    tag = '0.1.4',
-    dependencies = { 'nvim-lua/plenary.nvim' }
-  },
-  {
-    "christoomey/vim-tmux-navigator",
-    lazy = false,
-  }
+	{
+		"nvim-treesitter/nvim-treesitter",
+		event = { "BufReadPost", "BufNewFile" },
+		cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
+		build = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				auto_install = true,
+				highlight = {
+					enable = true,
+				},
+			})
+		end,
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		cmd = { "Telescope" },
+		tag = "0.1.4",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
+	{
+		"christoomey/vim-tmux-navigator",
+		lazy = false,
+	},
+	{
+		"jose-elias-alvarez/null-ls.nvim",
+		dependencies = {
+			"williamboman/mason.nvim",
+		},
+		event = "BufReadPre",
+		config = function()
+			local null_ls = require("null-ls")
+			local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+			local formatting = null_ls.builtins.formatting
+			local lint = null_ls.builtins.diagnostics
+			local sources = {
+				formatting.prettier,
+				formatting.stylua,
+				null_ls.builtins.formatting.gofmt,
+				null_ls.builtins.formatting.goimports,
+				lint.shellcheck,
+			}
+
+			-- Autoformatting on save
+			local on_attach = function(client, bufnr)
+				if client.supports_method("textDocument/formatting") then
+					vim.api.nvim_clear_autocmds({
+						group = augroup,
+						buffer = bufnr,
+					})
+
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						group = augroup,
+						buffer = bufnr,
+						callback = function()
+							vim.lsp.buf.format({ bufnr = bufnr })
+						end,
+					})
+				end
+			end
+
+			null_ls.setup({
+				debug = false,
+				sources = sources,
+				on_attach = on_attach,
+			})
+		end,
+	},
 }
