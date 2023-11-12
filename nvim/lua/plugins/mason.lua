b910160f756374c119b299a9017a52e1e17b4292
Reid Mason <maddogshain132@gmail.com>
2023-11-12 23:12:34 +0000

    Fixed formatting

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,70 +1,70 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		cmd = { "Mason", "MasonInstall", "MasonUninstall", "MasonUninstallAll", "MasonLog" },
-		event = { "BufReadPre", "BufNewFile" },
-		dependencies = {
-			"williamboman/mason-lspconfig.nvim",
-			"nvim/nvim-lspconfig",
-			"WhoIsSethDaniel/mason-tool-installer.nvim",
-		},
-		config = function()
-			require("mason").setup()
+  {
+    "williamboman/mason.nvim",
+    cmd = { "Mason", "MasonInstall", "MasonUninstall", "MasonUninstallAll", "MasonLog" },
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      "williamboman/mason-lspconfig.nvim",
+      "nvim/nvim-lspconfig",
+      "WhoIsSethDaniel/mason-tool-installer.nvim",
+    },
+    config = function()
+      require("mason").setup()
 
-			local lspconfig = require("lspconfig")
-			local mason_lsp_config = require("mason-lspconfig")
+      local lspconfig = require "lspconfig"
+      local mason_lsp_config = require "mason-lspconfig"
 
-			mason_lsp_config.setup_handlers({
-				-- Default handler
-				function(server)
-					lspconfig[server].setup({})
-				end,
+      mason_lsp_config.setup_handlers {
+        -- Default handler
+        function(server)
+          lspconfig[server].setup {}
+        end,
 
-				-- Dedicated handlers
-				["gopls"] = function()
-					lspconfig.gopls.setup({
-						cmd = { "gopls" },
-						settings = {
-							gopls = {
-								completeUnimported = true,
-								analyses = {
-									unusedparams = true,
-									usePlaceholders = false,
-									unusedvariable = true,
-									fieldalignment = true,
-									nilness = true,
-									shadow = true,
-									unusedwrite = true,
-								},
-							},
-						},
-					})
-				end,
+        -- Dedicated handlers
+        ["gopls"] = function()
+          lspconfig.gopls.setup {
+            cmd = { "gopls" },
+            settings = {
+              gopls = {
+                completeUnimported = true,
+                analyses = {
+                  unusedparams = true,
+                  usePlaceholders = false,
+                  unusedvariable = true,
+                  fieldalignment = true,
+                  nilness = true,
+                  shadow = true,
+                  unusedwrite = true,
+                },
+              },
+            },
+          }
+        end,
 
-				["lua_ls"] = function()
-					lspconfig.lua_ls.setup({
-						settings = {
-							Lua = {
-								format = {
-									enable = false,
-								},
-								diagnostics = {
-									globals = { "vim" },
-								},
-								workspace = {
-									library = {
-										[vim.fn.expand("$VIMRUNTIME/lua")] = true,
-										[vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
-										[vim.fn.stdpath("data") .. "/lazy/lazy.nvim/lua/lazy"] = true,
-									},
-									maxPreload = 100000,
-									preloadFileSize = 10000,
-								},
-							},
-						},
-					})
-				end,
-			})
-		end,
-	},
+        ["lua_ls"] = function()
+          lspconfig.lua_ls.setup {
+            settings = {
+              Lua = {
+                format = {
+                  enable = false,
+                },
+                diagnostics = {
+                  globals = { "vim" },
+                },
+                workspace = {
+                  library = {
+                    [vim.fn.expand "$VIMRUNTIME/lua"] = true,
+                    [vim.fn.expand "$VIMRUNTIME/lua/vim/lsp"] = true,
+                    [vim.fn.stdpath "data" .. "/lazy/lazy.nvim/lua/lazy"] = true,
+                  },
+                  maxPreload = 100000,
+                  preloadFileSize = 10000,
+                },
+              },
+            },
+          }
+        end,
+      }
+    end,
+  },
 }
