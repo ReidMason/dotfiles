116810d34ba83e5c969f16f2e960cf60bd4a4826
Reid Mason <maddogshain132@gmail.com>
2023-11-14 22:39:29 +0000

    Auto close nvim dap ui when exiting vim

--- nvim/lua/plugins/nvim-dap.lua
+++ nvim/lua/plugins/nvim-dap.lua
@@ -7,6 +7,10 @@ return {
     ft = "go",
     dependencies = "mfussenegger/nvim-dap",
     config = function(_, opts)
+      vim.api.nvim_create_user_command("DapUiClose", function()
+        require("dapui").close()
+      end, {})
+
       require("dap-go").setup(opts)
     end,
   },
