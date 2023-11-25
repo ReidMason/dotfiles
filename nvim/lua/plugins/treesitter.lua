5fbd4792e9a6c91cecb771e1cab8cc0c1ca88c79
Reid Mason <maddogshain132@gmail.com>
2023-11-25 15:55:26 +0000

    Added auto tag rename

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,14 +1,23 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  event = { "BufReadPost", "BufNewFile" },
-  cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
-  build = ":TSUpdate",
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      auto_install = true,
-      highlight = {
-        enable = true,
-      },
-    })
-  end,
+  {
+    "nvim-treesitter/nvim-treesitter",
+    dependencies = "windwp/nvim-ts-autotag",
+    event = { "BufReadPost", "BufNewFile" },
+    cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup {
+        auto_install = true,
+        autotag = {
+          enable = true,
+        },
+        highlight = {
+          enable = true,
+        },
+      }
+    end,
+  },
+  {
+    "windwp/nvim-ts-autotag",
+  },
 }
