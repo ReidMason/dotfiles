d520946a2ac38251a419145bc7d6c10bdb13a04c
Reid Mason <maddogshain132@gmail.com>
2024-01-29 23:35:20 +0000

    Re-ordered cmp sources

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -60,10 +60,10 @@ local function setup()
     },
     sources = cmp.config.sources {
       { name = "nvim_lsp" },
-      { name = "luasnip" },
       { name = "buffer" },
-      { name = "nvim_lua" },
       { name = "path" },
+      { name = "nvim_lua" },
+      { name = "luasnip" },
     },
   }
 
