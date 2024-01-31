3a746dbd93377cd3f8f099e29061fcc0577827a4
Reid Mason <maddogshain132@gmail.com>
2024-01-31 21:20:07 +0000

    Added oil.nvim hidden file exception for .zshrc

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -4,6 +4,11 @@ return {
     keymaps = {
       ["q"] = "actions.close",
     },
+    view_options = {
+      is_hidden_file = function(name, _)
+        return vim.startswith(name, ".") and name ~= ".zshrc"
+      end,
+    },
   },
   -- Optional dependencies
   dependencies = { "nvim-tree/nvim-web-devicons" },
