84a2211634b11e29462644de09b4133af163fce2
Reid Mason <maddogshain132@gmail.com>
2024-01-31 21:03:18 +0000

    Removed file_igore_patterns for telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -36,7 +36,6 @@ return {
           preview_cutoff = 120,
         },
         file_sorter = require("telescope.sorters").get_fuzzy_file,
-        file_ignore_patterns = { "node_modules", ".git", ".next" },
         generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
         path_display = { "truncate" },
         winblend = 0,
