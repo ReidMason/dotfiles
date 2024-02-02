6c966dc474fed8c94d7f60703124a24e389bb774
Reid Mason <maddogshain132@gmail.com>
2024-02-02 02:17:37 +0000

    Added browser markdown preview plugin

--- .config/nvim/lua/plugins/md-preview.lua
+++ .config/nvim/lua/plugins/md-preview.lua
@@ -1,6 +1,16 @@
 return {
-  dir = "~/Documents/repos/md-preview.nvim",
-  -- "reidmason/md-preview.nvim",
-  cmd = { "OpenMdPreview", "CloseMdPreview" },
-  opts = {},
+  {
+    dir = "~/Documents/repos/md-preview.nvim",
+    -- "reidmason/md-preview.nvim",
+    cmd = { "OpenMdPreview", "CloseMdPreview" },
+    opts = {},
+  },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+  },
 }
