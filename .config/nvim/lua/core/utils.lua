d25edba118c9a9c99dad612424a49c5c2ba0ad1b
Reid Mason <maddogshain132@gmail.com>
2024-03-03 12:35:52 +0000

    Added cmp capabilities

--- .config/nvim/lua/core/utils.lua
+++ .config/nvim/lua/core/utils.lua
@@ -21,6 +21,7 @@ end
 
 function M.get_capabilities()
   local capabilities = vim.lsp.protocol.make_client_capabilities()
+  capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 
   capabilities.textDocument.completion.completionItem = {
     documentationFormat = { "markdown", "plaintext" },
