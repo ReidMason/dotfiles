4bfd545e3ef1a0471dc8d8daffdfabb6b64ab776
Reid Mason <maddogshain132@gmail.com>
2024-03-20 22:41:18 +0000

    Reduced width and height for oil

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,13 +1,35 @@
 return {
   "stevearc/oil.nvim",
-  opts = {
-    keymaps = {
-      ["q"] = "actions.close",
-    },
-    view_options = {
-      show_hidden = true,
-    },
-  },
+  opts = function()
+    local HEIGHT_RATIO = 0.8
+    local WIDTH_RATIO = 0.8
+
+    local screen_w = vim.opt.columns:get()
+    local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+    local window_w = screen_w * WIDTH_RATIO
+    local window_h = screen_h * HEIGHT_RATIO
+    local window_w_int = math.floor(window_w)
+    local window_h_int = math.floor(window_h)
+
+    return {
+      float = {
+        -- Padding around the floating window
+        padding = 2,
+        max_width = window_w_int,
+        max_height = window_h_int,
+        border = "rounded",
+        win_options = {
+          winblend = 0,
+        },
+      },
+      keymaps = {
+        ["q"] = "actions.close",
+      },
+      view_options = {
+        show_hidden = true,
+      },
+    }
+  end,
   -- Optional dependencies
   dependencies = { "nvim-tree/nvim-web-devicons" },
   lazy = false,
