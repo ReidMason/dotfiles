a2d0ff6ad096fb64cd4e641b863d98ccc5610ed2
Reid Mason <maddogshain132@gmail.com>
2024-06-24 18:21:16 +0100

    Added git config to nix config

--- nix-darwin/home.nix
+++ nix-darwin/home.nix
@@ -107,6 +107,12 @@
     };
   };
 
+  programs.git = {
+    enable = true;
+    userName = "Reid Mason";
+    userEmail = "maddogshain132@gmail.com";
+  };
+
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
