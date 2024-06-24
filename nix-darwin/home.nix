06bfc02f964525c34fa4cd77cc708700c1830ec6
Reid Mason <maddogshain132@gmail.com>
2024-06-24 18:20:25 +0100

    Started replacing .zshrc with nix

--- nix-darwin/home.nix
+++ nix-darwin/home.nix
@@ -14,6 +14,8 @@
   home.stateVersion = "24.05"; # Please read the comment before changing.
 
   home.packages = [
+    pkgs.zsh
+    pkgs.eza
     pkgs.neofetch
     pkgs.git
     pkgs.starship
@@ -24,6 +26,27 @@
   home.file = {
     ".config/nvim" = {
       source = ../.config/nvim;
+    };
+  };
+  home.sessionVariables = {
+    EDITOR = "nvim";
+  };
+
+  programs.zsh = {
+    enable = true;
+    shellAliases = {
+      # Nix commands
+      rebuild-system = "sudo nixos-rebuild switch --flake ~/Documents/repos/dotfiles/nix-darwin/";
+      rebuild-home = "home-manager switch --flake ~/Documents/repos/dotfiles/nix-darwin/";
+
+      # Eza aliases
+      ls = "eza -lh --icons --no-user --group-directories-first";
+      ll = "eza -lah --icons --no-user --no-time --group-directories-first";
+      tree = "eza --tree --icons --no-user --no-time --group-directories-first";
+
+      # General aliases
+      cp = "cp -iv";
+      cd = "z";
     };
   };
 
