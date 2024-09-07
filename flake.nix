12c0f9ce20620e9ce01dd19fbd4dab9ea3a89ecc
Reid Mason <maddogshain132@gmail.com>
2024-09-07 13:23:54 +0100

    Changed home-manager to stable

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
         inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
