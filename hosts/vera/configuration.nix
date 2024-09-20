09bc840f2b719ce71ce5edb1e7f44f10ab7ed2eb
Reid Mason <maddogshain132@gmail.com>
2024-09-20 18:36:09 +0100

    feat: added flake support to vera

--- hosts/vera/configuration.nix
+++ hosts/vera/configuration.nix
@@ -25,6 +25,8 @@
   # Enable networking
   networking.networkmanager.enable = true;
 
+  nix.settings.experimental-features = [ "nix-command"  "flakes" ];
+
   # Set your time zone.
   time.timeZone = "Europe/London";
 
