0077f4ee2429c832f5494f1a2f37a4b88113e329
Reid Mason <maddogshain132@gmail.com>
2024-10-05 12:16:28 +0100

    fix: updated xkb variant configuration to fix warnings

--- hosts/vera/configuration.nix
+++ hosts/vera/configuration.nix
@@ -48,7 +48,7 @@
   # Configure keymap in X11
   services.xserver = {
     layout = "us";
-    xkbVariant = "";
+    xkb.Variant = "";
   };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
