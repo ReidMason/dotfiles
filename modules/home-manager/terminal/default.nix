ac0eb2ad9d171cdf923efeae927ed68c548ccf70
Reid Mason <maddogshain132@gmail.com>
2024-10-08 00:40:05 +0100

    feat: added lua as a dependency for neovim

--- modules/home-manager/terminal/default.nix
+++ modules/home-manager/terminal/default.nix
@@ -51,6 +51,7 @@ in
         (lib.optional config.terminal.neovim.enable pkgs.ripgrep)
         (lib.optional config.terminal.neovim.enable pkgs.lazygit)
         (lib.optional config.terminal.neovim.enable pkgs.clang)
+        (lib.optional config.terminal.neovim.enable pkgs.lua)
 
         (lib.optional config.terminal.just.enable pkgs.just)
     ];
