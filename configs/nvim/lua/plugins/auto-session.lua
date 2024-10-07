5a3e359b35af9582c1a2d9b1e6430ea730358457
Reid Mason <maddogshain132@gmail.com>
2024-10-08 00:15:36 +0100

    refactor: cleaned up close commands for auto session

--- configs/nvim/lua/plugins/auto-session.lua
+++ configs/nvim/lua/plugins/auto-session.lua
@@ -5,7 +5,7 @@ return {
     require("auto-session").setup {
       log_level = "error",
       auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
-      pre_save_cmds = { "tabdo NvimTreeClose", "tabdo DapUiClose", "CloseMdPreview" },
+      pre_save_cmds = { "tabdo DapUiClose" },
     }
   end,
   init = function()
