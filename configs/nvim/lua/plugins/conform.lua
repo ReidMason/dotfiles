c48f4433c9dc1d66944a3d4ed00a591d48ac53e6
Reid Mason <maddogshain132@gmail.com>
2024-12-02 01:22:26 +0000

    feat: added formatters

--- configs/nvim/lua/plugins/conform.lua
+++ configs/nvim/lua/plugins/conform.lua
@@ -4,9 +4,9 @@ return {
   opts = {
     formatters_by_ft = {
       lua = { "stylua" },
-      python = { "isort", "black" },
-      rust = { "rustfmt", lsp_format = "fallback" },
+      -- rust = { "rustfmt", lsp_format = "fallback" },
       javascript = { "prettierd", "prettier", stop_after_first = true },
+      astro = { "prettier" },
       haskell = { "fourmolu" },
       go = { "gofmt", "goimports" },
     },
