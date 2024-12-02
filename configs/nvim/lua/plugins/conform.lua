0ced580a857b2c13c5c58ce2bd2cf7a58672dabf
Reid Mason <maddogshain132@gmail.com>
2024-12-02 00:54:16 +0000

    feat: replaced null-ls with conform

---
+++ configs/nvim/lua/plugins/conform.lua
@@ -0,0 +1,19 @@
+return {
+  "stevearc/conform.nvim",
+  event = "BufReadPre",
+  opts = {
+    formatters_by_ft = {
+      lua = { "stylua" },
+      python = { "isort", "black" },
+      rust = { "rustfmt", lsp_format = "fallback" },
+      javascript = { "prettierd", "prettier", stop_after_first = true },
+      haskell = { "fourmolu" },
+      go = { "gofmt", "goimports" },
+    },
+    format_on_save = {
+      -- These options will be passed to conform.format()
+      timeout_ms = 500,
+      lsp_format = "fallback",
+    },
+  },
+}
