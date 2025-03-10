4814b2bb5d131a21d204de1cebee32fd0a32c6b7
Reid Mason <maddogshain132@gmail.com>
2025-03-10 23:20:40 +0000

    fix: migrated tmux catppuccin config

--- modules/home-manager/terminal/tmux.nix
+++ modules/home-manager/terminal/tmux.nix
@@ -20,10 +20,6 @@ let
           # Enable full rgb colours
           set-option -sa terminal-overrides ",xterm*:Tc"
 
-          set-option -g status-interval 5
-          # Don't detach when destroying a session
-          set-option -g detach-on-destroy off 
-
           # Split panes
           bind h split-window -v -c "#{pane_current_path}"
           bind v split-window -h -c "#{pane_current_path}"
@@ -47,6 +43,8 @@ let
           # General config
           set -g renumber-windows on    # Renumber all windows when any window is closed
           set -g set-clipboard on       # Use system clipboard
+
+          set-option -g status-interval 5
           set -g status-interval 2      # Update status every 2 seconds
           set -g status-position top    # macOS / darwin style
 
@@ -73,23 +71,26 @@ let
           {
             plugin = pkgs-unstable.tmuxPlugins.catppuccin;
             extraConfig = ''
+              set -g @catppuccin_flavor "mocha"
+
+              set -g @catppuccin_window_status_style "custom"
               set -g @catppuccin_window_left_separator " "
               set -g @catppuccin_window_right_separator " "
               set -g @catppuccin_window_middle_separator " "
 
-              set -g @catppuccin_window_default_fill "all"
-              set -g @catppuccin_window_default_color "bg"
-              set -g @catppuccin_window_default_background "none"
-              set -g @catppuccin_window_default_text "#W"
+              set-window-option -g window-status-current-style "fg=#{@thm_pink}"
+              set-window-option -g window-status-style "fg=#{@thm_overlay_2}"
+              set -g @catppuccin_window_text_color "none"
+              set -g @catppuccin_window_number_color "none"
+              set -g @catppuccin_window_current_text_color "none"
+              set -g @catppuccin_window_current_number_color "none"
 
-              set -g @catppuccin_window_current_fill "all"
-              set -g @catppuccin_window_current_color "magenta"
-              set -g @catppuccin_window_current_background "none"
+              set -g @catppuccin_window_text "#W"
               set -g @catppuccin_window_current_text "#W"
 
-              set -g @catppuccin_status_modules_right "session"
-              set -g @catppuccin_status_left_separator  " "
-              set -g @catppuccin_status_right_separator ""
+              set -g @catppuccin_status_background "none"
+              set -g status-right "#{E:@catppuccin_status_session}"
+              set -g status-left ""
             '';
           }
           pkgs-unstable.tmuxPlugins.sensible
