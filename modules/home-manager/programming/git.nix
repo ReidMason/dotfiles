e0f8b96fd5a661ea30d4b8a5bacf984088b41862
Reid Mason <maddogshain132@gmail.com>
2025-04-28 09:31:55 +0100

    feat: added git alias

--- modules/home-manager/programming/git.nix
+++ modules/home-manager/programming/git.nix
@@ -1,19 +1,23 @@
-{pkgs, config, lib, options, parent-name, ...}:
+{ pkgs, config, lib, options, parent-name, ... }:
 let
-module = {
-  module-name = "git";
-  label = "Git";
-  config = {
-    programs.git = {
-      enable = true;
-      userName = "Reid Mason";
-      userEmail = "maddogshain132@gmail.com";
-      aliases = {
-        lg = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)' --all";
+  module = {
+    module-name = "git";
+    label = "Git";
+    config = {
+      programs.git = {
+        enable = true;
+        userName = "Reid Mason";
+        userEmail = "maddogshain132@gmail.com";
+        aliases = {
+          lg = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)' --all";
+        };
+      };
+
+      home.shellAliases = {
+        g = "git";
       };
     };
   };
-};
 in
 {
   imports = [
