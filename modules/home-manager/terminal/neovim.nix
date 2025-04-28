c4e6987b573add5be67ee0c4faa73ced2f47cae2
Reid Mason <maddogshain132@gmail.com>
2025-04-28 09:58:41 +0100

    feat: added alias for neovim

--- modules/home-manager/terminal/neovim.nix
+++ modules/home-manager/terminal/neovim.nix
@@ -1,28 +1,32 @@
-{pkgs, pkgs-unstable, config, lib, options, parent-name, ...}:
+{ pkgs, pkgs-unstable, config, lib, options, parent-name, ... }:
 let
-module = {
-  module-name = "neovim";
-  label = "Neovim";
-  config = {
-    home.packages = [
+  module = {
+    module-name = "neovim";
+    label = "Neovim";
+    config = {
+      home.packages = [
         pkgs-unstable.neovim
         pkgs.ripgrep
         pkgs.lazygit
         # pkgs.clang
         pkgs.lua
-    ];
+      ];
 
-    home.sessionVariables = {
-       EDITOR = "nvim";
-    };
+      home.sessionVariables = {
+        EDITOR = "nvim";
+      };
 
-    home.file = {
-      ".config/nvim" = {
-        source = ../../../configs/nvim;
+      home.file = {
+        ".config/nvim" = {
+          source = ../../../configs/nvim;
+        };
+      };
+
+      home.shellAliases = {
+        v = "nvim";
       };
     };
   };
-};
 in
 {
   imports = [
