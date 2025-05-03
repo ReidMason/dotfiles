6918a353546fe23e4e868c3cc2746a9e5d3a15ca
Reid Mason <maddogshain132@gmail.com>
2025-05-03 12:55:02 +0100

    refactor: split golang into it's own module

--- modules/home-manager/programming/default.nix
+++ modules/home-manager/programming/default.nix
@@ -9,26 +9,21 @@ in
     (import ./haskell.nix { inherit pkgs pkgs-unstable lib options config; parent-name = module-name; })
     (import ./node.nix { inherit pkgs pkgs-unstable lib options config; parent-name = module-name; })
     (import ./dotnet.nix { inherit pkgs pkgs-unstable lib options config; parent-name = module-name; })
+    (import ./golang.nix { inherit pkgs pkgs-unstable lib options config; parent-name = module-name; })
   ];
 
   options.programming = {
     enable = lib.mkEnableOption "Enable all programming tools";
 
-    golang.enable = lib.mkEnableOption "Enable Golang";
     lazygit.enable = lib.mkEnableOption "Enable Lazygit";
     rust.enable = lib.mkEnableOption "Enable rust";
   };
 
   config = {
-    programming.golang.enable = lib.mkDefault config.programming.enable;
     programming.lazygit.enable = lib.mkDefault config.programming.enable;
     programming.rust.enable = lib.mkDefault config.programming.enable;
 
     home.packages = lib.concatLists [
-      (lib.optional config.programming.golang.enable pkgs.go)
-      (lib.optional config.programming.golang.enable pkgs.sqlc)
-      (lib.optional config.programming.golang.enable pkgs.goose)
-
       (lib.optional config.programming.rust.enable pkgs.cargo)
 
       (lib.optional config.programming.lazygit.enable pkgs.lazygit)
---
+++ modules/home-manager/programming/golang.nix
@@ -0,0 +1,20 @@
+{ pkgs, config, lib, options, parent-name, ... }:
+let
+  module = {
+    module-name = "golang";
+    label = "Golang";
+    config = {
+
+      home.packages = with pkgs; [
+        go
+        sqlc
+        goose
+      ];
+    };
+  };
+in
+{
+  imports = [
+    (import ../module-setup.nix { inherit config lib parent-name module; })
+  ];
+}
