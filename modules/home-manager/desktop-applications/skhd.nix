6f8967a3e2481f48d63d08eefb443193b883894d
Reid Mason <maddogshain132@gmail.com>
2025-05-21 17:29:48 +0100

    feat: added skhd keybinds

--- modules/home-manager/desktop-applications/skhd.nix
+++ modules/home-manager/desktop-applications/skhd.nix
@@ -1,4 +1,4 @@
-{ pkgs, config, lib, options, parent-name, ... }:
+{ pkgs, pkgs-unstable, config, lib, options, parent-name, ... }:
 let
   module = {
     module-name = "skhd";
@@ -8,8 +8,21 @@ let
         pkgs.skhd
       ];
 
-      services.skhd.enable = true;
-      # services.skhd.config = { };
+      home.file.".skhdrc".text = ''
+        # These are binds set in the macos keybinds (System Settings > Accessibility > Keyboard Shortcuts > Windows > Halves)
+        # You have to change these binds because by default they are "fn + ctrl + arrows"
+        # That overlaps with page up/down, home and end because macos is dumb and you can't bind these in skhd
+        # You can just go into the settings, change the binding then hit the skhd shortcut and it'll fill the binding for you
+        alt - f : skhd -k "fn + ctrl - f"
+        alt - h : skhd -k "alt + shift + ctrl - h"
+        alt - j : skhd -k "alt + shift + ctrl - j"
+        alt - k : skhd -k "alt + shift + ctrl - k"
+        alt - l : skhd -k "alt + shift + ctrl - l"
+
+        # Applications
+        alt - b : open /Applications/Arc.app
+        alt - t : open "${pkgs-unstable.wezterm}/Applications/WezTerm.app"
+      '';
     };
   };
 in
