b8c165039ceb670e1f8a1602b53c13375ab30ba6
Reid Mason <maddogshain132@gmail.com>
2026-03-17 11:51:51 +0000

    fix: corrected bootloader

--- hosts/github-runner/hardware-configuration.nix
+++ hosts/github-runner/hardware-configuration.nix
@@ -5,7 +5,11 @@
     (modulesPath + "/profiles/qemu-guest.nix")
   ];
 
-  boot.loader.systemd-boot.enable = true;
+  boot.loader.grub = {
+    enable = true;
+    efiSupport = true;
+    device = "nodev";
+  };
   boot.loader.efi.canTouchEfiVariables = true;
 
   boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "virtio_pci" "sr_mod" "virtio_blk" ];
