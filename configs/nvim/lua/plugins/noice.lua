97201574842210f8d2ee51d83e148b3adf27a87a
Reid Mason <maddogshain132@gmail.com>
2026-06-29 13:56:08 +0100

    refactor: cleaned up noice to remove cmp references

--- configs/nvim/lua/plugins/noice.lua
+++ configs/nvim/lua/plugins/noice.lua
@@ -11,11 +11,10 @@ return {
           enabled = false,
         },
       },
-      -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+      -- override markdown rendering so that other plugins use **Treesitter**
       override = {
         ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
         ["vim.lsp.util.stylize_markdown"] = true,
-        ["cmp.entry.get_documentation"] = true,
       },
     },
     presets = {
