1ded0b9a6568ec83119b40cd58c0f764cf9d58ea
Reid Mason <maddogshain132@gmail.com>
2026-07-04 23:19:38 +0100

    fix: added default configs for obsidian to fix templates etc

--- configs/nvim/lua/plugins/obsidian.lua
+++ configs/nvim/lua/plugins/obsidian.lua
@@ -5,27 +5,48 @@ return {
   cmd = { "Obsidian" },
   ft = "markdown",
   ---@module 'obsidian'
-  ---@type obsidian.config
-  opts = {
-    frontmatter = {
-      enabled = false,
-    },
-    legacy_commands = false, -- this will be removed in 4.0.0
-    ui = {
-      -- render-markdown.nvim handles vault UI
-      enable = false,
-    },
-    picker = {
-      name = "snacks.picker",
-    },
-    completion = {
-      min_chars = 1,
-    },
-    workspaces = {
-      {
-        name = "personal",
-        path = "~/Documents/Knowledge Base",
+  ---@type fun(): obsidian.config
+  opts = function()
+    ---@type obsidian.config.TemplateOpts
+    local templates = vim.tbl_deep_extend("force", require("obsidian.config.default").templates, {
+      folder = "005 - Templates",
+    })
+
+    return {
+      frontmatter = {
+        enabled = false,
       },
-    },
-  },
+      legacy_commands = false, -- this will be removed in 4.0.0
+      ui = {
+        -- render-markdown.nvim handles vault UI
+        enable = false,
+      },
+      picker = {
+        name = "snacks.picker",
+      },
+      completion = {
+        min_chars = 1,
+      },
+      templates = templates,
+      notes_subdir = "002 - Cortex",
+      new_notes_location = "notes_subdir",
+      daily_notes = {
+        folder = "002 - Cortex",
+        template = "Journal",
+        workdays_only = false,
+      },
+      link = {
+        auto_update = true,
+      },
+      file = {
+        ignore_filters = { ".obsidian", ".git" },
+      },
+      workspaces = {
+        {
+          name = "personal",
+          path = "~/Documents/Knowledge Base",
+        },
+      },
+    }
+  end,
 }
