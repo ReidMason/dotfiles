2386b985eb950d9c7dbaf5ff27398d44a6024884
Reid Mason <maddogshain132@gmail.com>
2026-07-04 22:33:06 +0100

    feat: moved knowledge base location

--- configs/nvim/lua/plugins/obsidian.lua
+++ configs/nvim/lua/plugins/obsidian.lua
@@ -24,7 +24,7 @@ return {
     workspaces = {
       {
         name = "personal",
-        path = "~/Documents/Knowledge Base/Knowledge Base",
+        path = "~/Documents/Knowledge Base",
       },
     },
   },
